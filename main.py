3a012741e6748b94bc14c4632381771d576cb07d
99T-afk <s5117818@bournemouth.ac.uk>
2022-04-29 21:10:30 +0100

    Connect to twitter via API

--- main.py
+++ main.py
@@ -2,15 +2,30 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tweepy
+from passwrd import *
 
+#
+#
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def connectToTwitter():
+    print("Connecting to twitter")
+    #auth = tweepy.OAuth1UserHandler(notMyAPIKey,notMyAPIKeySecret,notMyAccessToken,notMyAccessTokenSecret)
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(notMyAPIKey, notMyAPIKeySecret)
+    auth.set_access_token(notMyAccessToken, notMyAccessTokenSecret)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    status = "Hi Lisa!"
+    api.update_status(status=status)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    connectToTwitter()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
